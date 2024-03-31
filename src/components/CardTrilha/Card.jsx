1e9f5047d90c15d113b165916bd261549bfcd1ce
Alana IFSC <alana.brm@aluno.ifsc.edu.br>
2024-03-30 23:00:10 -0300

    Exercicios da semana 10 finalizados

---
+++ src/components/CardTrilha/Card.jsx
@@ -0,0 +1,43 @@
+// import React from 'react';
+// import './Card.css'; // Arquivo de estilos CSS para o card
+// import Star from './Avaliacao';
+
+// const items: number[] = [...Card(new Array(5).keys() as any)];
+
+// const Card = ({ title, content }) => {
+//     return (
+//         <div className="card">
+//             <h2>{title}</h2>
+//             <div className="card-content">
+//                 <p id='line'>{content}</p>
+//                 <p id='por'>{content}</p>
+//             </div>
+//             <div className='features'>
+//                 <p id='duracao'>{content}</p>
+//                 <p id='trajeto'>{content}</p>
+//             </div>
+//             <div className='badge'>
+//                 <label id='badge'>{content}</label>
+//             </div>
+//             <div className='avaliacao'>
+//                 <div className='estrelasetexto'>
+//                     <div className='estrelas'>
+//                         <Star isActived />
+//                         <Star isActived />
+//                         <Star isActived />
+//                         <Star isActived />
+//                         <Star isActived />
+//                         <span>{content}</span>
+//                     </div>
+//                 </div>
+//             </div>
+
+
+
+
+
+//         </div>
+//     );
+// }
+
+// export default Card;
---
+++ src/components/CardTrilha/index.jsx
@@ -0,0 +1,41 @@
+import * as PropTypes from 'prop-types';
+import './cardTrilha.css';
+
+function CardTrilha({ dadosTrilha }) {
+
+    return (
+        <div className="card_container">
+            <div className="card_img">
+                <img className="card_imagem" width={200} src={dadosTrilha.urlImagem} alt="Imagem da trilha" />
+            </div>
+            <div className="card_info" >
+                <h1 className='card_titulo'>{dadosTrilha.nomeTrilha} - {dadosTrilha.cidade} - {dadosTrilha.estado}</h1>
+
+                <p>_____________</p>
+                <p id="autor">Por:{dadosTrilha.nomeUsuario}</p>
+                <p>Duração: {dadosTrilha.duracao} min</p>
+                <p>Distância: {dadosTrilha.trajeto} km</p>
+                <p>Dificuldade: {dadosTrilha.dificuldade}</p>
+                <p>Tipo: {dadosTrilha.tipo}</p>
+
+            </div>
+        </div>
+    );
+
+}
+
+CardTrilha.propTypes = {
+    dadosTrilha: PropTypes.shape({
+        nomeTrilha: PropTypes.string.isRequired,
+        cidade: PropTypes.string.isRequired,
+        estado: PropTypes.string.isRequired,
+        duracao: PropTypes.string.isRequired,
+        trajeto: PropTypes.string.isRequired,
+        dificuldade: PropTypes.string.isRequired,
+        tipo: PropTypes.oneOf(['caminhada', 'trakkking', 'corrida', 'ciclismo']).isRequired,
+        nomeUsuario: PropTypes.string.isRequired,
+        urlImagem: PropTypes.string.isRequired
+    })
+}
+
+export default CardTrilha;

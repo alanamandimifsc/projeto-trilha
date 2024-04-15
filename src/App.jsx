21c04792460ef7e62b08277ee515573a86b86010
Alana IFSC <alana.brm@aluno.ifsc.edu.br>
2024-04-14 23:06:39 -0300

    Implementando pagina inicial

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import Header from "./components/header";
 import { Outlet } from "react-router-dom";
 import { TrilhasContextProvider } from "./context/TrilhasContext"
+import Footer from "./components/atoms/footer/footer";
 
 function App() {
   return (
     <TrilhasContextProvider>
       <Header />
       <Outlet />
+      <Footer />
     </TrilhasContextProvider>
   );
 }
